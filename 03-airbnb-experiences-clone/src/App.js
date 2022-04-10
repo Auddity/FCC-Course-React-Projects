5387887153caf67ef7677fea7fa9f45f16f2480f
Auddity <audsecondarylap@outlook.com>
2022-04-10 14:56:27 -0400

    navbar

--- 03-airbnb-experiences-clone/src/App.js
+++ 03-airbnb-experiences-clone/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Nav />
       </header>
     </div>
   );
---
+++ 03-airbnb-experiences-clone/src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from '../images/logo.svg';
+
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <img src={logo} alt="airbnb log" />
+    </nav>
+  )
+}
+
+export default Nav
