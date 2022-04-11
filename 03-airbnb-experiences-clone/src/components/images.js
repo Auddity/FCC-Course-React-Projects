20d62e5ab5abc91fed6e9bc2b2ba4d885b8805cd
Auddity <audsecondarylap@outlook.com>
2022-04-10 20:06:05 -0400

    picture grid

--- 03-airbnb-experiences-clone/src/components/images.js
+++ 03-airbnb-experiences-clone/src/components/images.js
@@ -1,7 +1,7 @@
 const images = [
   { id: 1, title: 'pic1', src: './images/Left 1.png', alt: 'cooking pan'},
-  { id: 2, title: 'pic2', src: './images/Left Bottom 2.png', alt: 'dancer'},
-  { id: 3, title: 'pic3', src: './images/Left Top 2.png', alt: 'baker'},
+  { id: 2, title: 'pic2', src: './images/Left Top 2.png', alt: 'baker'},
+  { id: 3, title: 'pic3', src: './images/Left Bottom 2.png', alt: 'dancer'},
   { id: 4, title: 'pic4', src: './images/Middle Bottom 3.png', alt: 'guitar'},
   { id: 5, title: 'pic5', src: './images/Middle Top 3.png', alt: 'salad'},
   { id: 6, title: 'pic6', src: './images/Right Top 4.png', alt: 'keyboard player'},
