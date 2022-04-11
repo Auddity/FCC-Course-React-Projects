7fddd37a47b58b0e76bdfbeeaf0d2acbff3993d5
Auddity <audsecondarylap@outlook.com>
2022-04-11 18:12:15 -0400

    hero text

--- 03-airbnb-experiences-clone/src/components/Main.js
+++ 03-airbnb-experiences-clone/src/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import images from './images.js';
+import '../css/Main.css'
 import '../css/grid.css';
 
 
@@ -9,6 +10,8 @@ const Main = () => {
       <div className="gridContainer">
         { images.map(({ id, title, src, alt }) => <img key={id} src={src} title={title} alt={alt} />) }
       </div>
+      <h1>Online Experiences</h1>
+      <p>Join unique interactive ativites led by one-of-a-kind hosts-all whith out leaving home.</p>
     </main>
   )
 }
