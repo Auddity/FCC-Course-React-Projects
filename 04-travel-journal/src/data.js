38682eb4a759e9b10a37b85764e58256165065c2
Auddity <audsecondarylap@outlook.com>
2022-04-21 11:44:31 -0400

    data file

---
+++ 04-travel-journal/src/data.js
@@ -0,0 +1,37 @@
+const data = [
+  {
+    title: "Niagara Falls",
+    location: "USA / Canada",
+    mapLink: "https://goo.gl/maps/7nHFR631e5RfMCvi6",
+    dates: {
+      startDate: "May 20th, 2000",
+      endDate: "May 30th, 2000",
+    },
+    description: "Niagara Falls is a gorup of three waterfalls at the southern end of Niagara Gorge, spanning the border between the province of Ontario in Canada and the state of New York in the United States.",
+    imageSource: "https://unsplash.com/photos/9pGqF6zSBb0",
+  },
+  {
+    title: "Bern",
+    location: "Bern, Switzerland",
+    mapLink: "https://goo.gl/maps/dJzRth4mTetd5eSh8",
+    dates: {
+      startDate: "July 1st, 2006",
+      endDate: "July 30th, 2006",
+    },
+    description: "Bern is the de facto capital of Switzerland. It lies along a narrow loop of the Aare River.  It was founded in 1191 as a military post on the frontier between the German-speaking Alemanni and the French-speaking inhabitants of Burgandy.",
+    imageSource: "https://unsplash.com/photos/L-4pw7UPMKA",
+  },
+  {
+    title: "Lion Monuement",
+    location: "Lucerne, Switzerland",
+    mapLink: "https://goo.gl/maps/SNLLKDW9P7qJhcGh8",
+    dates: {
+      startDate: "July 20th, 2006",
+      endDate: "July 21st, 2006",
+    },
+    description: "The Lion Monument in Lucerne is a giant dying lion carved out of a wall of sandstone rock above a pond at the east end of the medieveal town. It was designed as a memorial for the mercenary soldiers from central Switzerland who lost their lives while serving the French king Louis XVI during the French Revolution",
+    imageSource: "https://unsplash.com/photos/7iUzITtW35k",
+  }
+];
+
+export default data;
