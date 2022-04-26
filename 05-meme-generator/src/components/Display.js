fa2992ea9735fe586228e2959be0a4c0c2ffbe70
Auddity <audsecondarylap@outlook.com>
2022-04-26 14:47:16 -0400

    display element

---
+++ 05-meme-generator/src/components/Display.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Display.css';
+
+const Display = () => {
+  return (
+    <section className="Display">
+      
+    </section>
+  )
+}
+
+export default Display
--- 05-meme-generator/src/components/Main.js
+++ 05-meme-generator/src/components/Main.js
@@ -1,11 +1,13 @@
 import React from "react";
 import '../css/Main.css';
 import Form from './InputForm';
+import Display from './Display';
 
 const Main = () => {
   return (
-    <main className="main">
+    <main className="Main">
       <Form />
+      <Display />
     </main>
   )
 }
